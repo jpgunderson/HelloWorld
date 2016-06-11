c6f88806300f85541d5dbc2ec223b943a630ed50
Jim Gunderson <jpgunderson@GunderFish.com>
2016-06-11 06:21:39 -0600

    added Encabulator

--- HelloWorld/src/com/gunderfish/Hello.java
+++ HelloWorld/src/com/gunderfish/Hello.java
@@ -1,13 +1,18 @@
 package com.gunderfish;
 
 public class Hello {
+	
+	Encabulator eBooster;
 
 	public Hello() {
-		// TODO Auto-generated constructor stub
+		eBooster = new Encabulator();
+		eBooster.setTurbo(false);
 	}
 	
 	public static void main(String[] args) {
+		Hello left = new Hello();
 		System.out.println("Hello World! 6");
+		System.out.println("Encabulator State: " + left.eBooster.isTurboActive());
 	}
 
 }
