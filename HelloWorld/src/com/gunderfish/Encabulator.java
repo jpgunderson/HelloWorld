ea8bd218bff688fff95c8798b1f4cc79ed8ba771
Jim Gunderson <jpgunderson@GunderFish.com>
2016-06-11 06:27:20 -0600

    Will Encabulator make it?

---
+++ HelloWorld/src/com/gunderfish/Encabulator.java
@@ -0,0 +1,18 @@
+package com.gunderfish;
+
+public class Encabulator {
+	boolean bUseTurbo = false;
+
+	public Encabulator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setTurbo(boolean on) {
+		bUseTurbo = on;
+	}
+	
+	public boolean isTurboActive() {
+		return bUseTurbo;
+	}
+
+}
