66f718f5181ac5371be61ef323ebe62a4c62efa6
Jim Gunderson <jpgunderson@GunderFish.com>
2016-06-10 20:17:56 -0600

    Test

---
+++ HelloWorld/src/com/gunderfish/Hello.java
@@ -0,0 +1,13 @@
+package com.gunderfish;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
